1305b0c4a51a3adc3c08ac48659f615cc591aa8c
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2023-06-27 16:38:42 -0600

    Stop background scrolling in game over added

--- app.py
+++ app.py
@@ -18,20 +18,24 @@ class Bird(pygame.sprite.Sprite):
         self.rect.center = [x, y] #Le damos su posición
         self.velocity_y = 0
         self.flying = False
+        self.game_over = False
         
     def animation(self):
         self.movement()
-        self.counter += 1
-        #Hacemos un cooldown antes de cambiar de imagen
-        if self.counter > self.cooldown:
-            self.counter = 0
-            self.index += 1 #Aumentamos nuestro índice para recorrer las imágenes
-            if self.index >= len(self.images):
-                self.index = 0
-        self.image = self.images[self.index]
-        
-        #Rotamos la imagen cuandi este de caída
-        self.image = pygame.transform.rotate(self.images[self.index], self.velocity_y * -2)
+        if self.game_over == False:
+            self.counter += 1
+            #Hacemos un cooldown antes de cambiar de imagen
+            if self.counter > self.cooldown:
+                self.counter = 0
+                self.index += 1 #Aumentamos nuestro índice para recorrer las imágenes
+                if self.index >= len(self.images):
+                    self.index = 0
+            self.image = self.images[self.index]
+            
+            #Rotamos la imagen cuandi este de caída
+            self.image = pygame.transform.rotate(self.images[self.index], self.velocity_y * -2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
     
     def movement(self):
         #En nuestro pajara tenemos dos fuerzas que están actuando,
@@ -45,12 +49,13 @@ class Bird(pygame.sprite.Sprite):
         
         #Jump
         #Mientras sea menor el valor "y" del pajaro a 504 ira aumentado en "y"
-       if self.rect.bottom < 504: 
-            self.rect.y += int(self.velocity_y) 
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_SPACE] and self.rect.y > 0:
-                self.rect.y -= 10
-                print("AARINNA")
+       if not self.game_over:
+            if self.rect.bottom < 504: 
+                self.rect.y += int(self.velocity_y) 
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE] and self.rect.y > 0:
+                    self.rect.y -= 10
+                    print("AARINNA")
 
 def draw_game(screen, bg, bg2, scroll, bird_arr, bird):
     #Draw Background
@@ -93,13 +98,19 @@ def run():
     running = True
     while running:
         #Frame Rate
-        clock.tick(60)
+        clock.tick(FPS)
         
         #Scroll Background
-        ground_scroll -= scroll_speed
-        if abs(ground_scroll) > 35:
-            ground_scroll = 0
-        
+        if not flappy.game_over:
+            #Si el juego llega estar en game over ya no habra scroll
+            ground_scroll -= scroll_speed
+            if abs(ground_scroll) > 35:
+                ground_scroll = 0
+            
+        #Game Over
+        if flappy.rect.bottom >= 504:
+            flappy.game_over = True
+            flappy.flying = False
 
         draw_game(screen, background, background_floor, ground_scroll, bird_group, flappy)
         
@@ -108,7 +119,7 @@ def run():
             if event.type == pygame.QUIT:
                 running = False
             #JUMP
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN and not flappy.game_over:
                 if event.key == pygame.K_SPACE:
                  flappy.movement()
                  flappy.flying = True
